c9d8c6976a1f8f93c2a4213151e9a9c927752434
Muhammad Zachrie Kurniawan <zachrie2005@gmail.com>
2025-07-13 21:26:51 +0700

    fix: input background

--- src/components/text-input/index.tsx
+++ src/components/text-input/index.tsx
@@ -21,7 +21,7 @@ const TextInput: React.FC<TextInputProps> = ({ label, name, value, onChange, req
 				value={value}
 				onChange={(e) => onChange(e.target.value)}
 				required={required}
-				className="mt-1 block w-full px-3 py-2 border rounded-md shadow-sm dark:bg-gray-700 dark:text-white border-gray-300 dark:border-gray-600 focus:outline-none focus:ring-2 focus:ring-blue-500"
+				className="mt-1 block w-full px-3 py-2 border rounded-md shadow-sm bg-white dark:bg-gray-700 dark:text-white border-gray-300 dark:border-gray-600 focus:outline-none focus:ring-2 focus:ring-blue-500"
 			/>
 			{!errors ? null : (
 				<div className="mt-1">
